89857fcde77a32a974492fc67346e9804c04f74c
YukoKano <halloween_1031@hotmail.co.jp>
2023-07-20 22:08:47 +0900

    test

---
+++ 01_firstProject/src/MyButton.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+
+export function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    // alert('You clicked me!');
+    setCount(count + 1);
+  }
+
+  return (
+    // <button onClick={handleClick}>Clicked {count} times</button>
+    <OtherButton count={count} onClick={handleClick} />
+  );
+}
+
+
+
+export function OtherButton({ count, onClick }) {
+  return (
+    <button onClick={onClick}>
+      { count } times
+    </button>
+  );
+}
